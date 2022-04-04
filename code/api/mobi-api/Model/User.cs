f0815027113df5debcd001ddc86f0566bc20cd81
Leo LI <xin.leo.li@gmail.com>
2022-04-04 18:00:30 +1200

    add api project

---
+++ code/api/mobi-api/Model/User.cs
@@ -0,0 +1,11 @@
+namespace mobi_api
+{
+    public class Company
+    {
+        public DateTime Date { get; set; }
+
+        public string? CompanyName { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
