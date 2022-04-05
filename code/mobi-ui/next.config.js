c77e8d0fe707e6877d8cd57f00293dc864f902d3
leo li <xin.leo.li@gmail.com>
2022-04-05 14:28:53 +1200

    fixing bulma

--- code/mobi-ui/next.config.js
+++ code/mobi-ui/next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
 const path = require('path');
-const nextConfig = {
-  reactStrictMode: true,
+
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
-
-module.exports = nextConfig;
--- code/mobi-ui/pages/profile.tsx
+++ code/mobi-ui/pages/profile.tsx
@@ -4,6 +4,20 @@ const Profile: NextPage = () => {
   return (
     <div>
       <h2 className="title is-2">Name: Leo</h2>
+
+      <div className="columns">
+        <div className="column">First column</div>
+        <div className="column">Second column</div>
+        <div className="column">Third column</div>
+        <div className="column">Fourth column</div>
+      </div>
+
+      <div className="buttons">
+        <button className="button is-info">Info</button>
+        <button className="button is-success">Success</button>
+        <button className="button is-warning">Warning</button>
+        <button className="button is-danger">Danger</button>
+      </div>
     </div>
   );
 };
