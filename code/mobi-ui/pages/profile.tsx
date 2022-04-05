4a1dca96db05e871e490882a16440d05ec9670b7
leo li <xin.leo.li@gmail.com>
2022-04-05 12:41:47 +1200

    Add new page and add readme

---
+++ code/mobi-ui/pages/profile.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Profile: NextPage = () => {
+  return (
+    <div>
+      <h2 className="title is-2">Name: Leo</h2>
+    </div>
+  );
+};
+
+export default Profile;
