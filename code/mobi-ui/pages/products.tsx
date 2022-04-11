e60a84280f97294b62b458a2f49983dcf148fe82
Jimmy MO <jimmymonz@gmail.com>
2022-04-12 09:08:10 +1200

    adjust space between columns

--- code/mobi-ui/pages/products.tsx
+++ code/mobi-ui/pages/products.tsx
@@ -3,16 +3,19 @@ const Profile: NextPage = () => {
   return (
     <div class="columns">
       <div class="column">
-        <p class="bd-notification is-danger">Store Name</p>
+        <h1 class="bd-notification is-full">Store Name</h1>
         <div class="columns is-mobile">
-          <div class="column is-half">
+          <div class="column is-one-third">
             <p class="bd-notification is-danger">Product</p>
+            <p>Example product</p>
           </div>
-          <div class="column">
+          <div class="column is-half">
             <p class="bd-notification is-danger">Description</p>
+            <p>Example Description</p>
           </div>
-          <div class="column">
+          <div class="column is-one-fifth">
             <p class="bd-notification is-danger">Price</p>
+            <p>example price</p>
           </div>
         </div>
       </div>
