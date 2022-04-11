66bb344fba5d19e0f780c2a50a641d550e75cb82
Mangud1990 <52785924+Mangud1990@users.noreply.github.com>
2022-04-11 21:38:09 +1200

    added login componant

--- code/mobi-ui/pages/index.tsx
+++ code/mobi-ui/pages/index.tsx
@@ -1,7 +1,31 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+import Head from "next/head";
+import LoginForm from "../components/login-form";
 
 const Home: NextPage = () => {
-  return <div className="tile">Welcome</div>;
+  return (
+    <div>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+      <main>
+        {/* <div className="columns is-mobile">
+          <div className="column">1</div>
+          <div className="column">2</div>
+          <div className="column">
+            
+          </div>
+          <div className="column">4</div>
+        </div> */}
+
+        <div className="columns is-mobile">
+          <div className="column is-three-fifths is-offset-one-fifth">
+            <LoginForm></LoginForm>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
