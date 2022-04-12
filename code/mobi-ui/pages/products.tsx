56195dfb26f1ac1c06278c9e22658e21bb932d92
leo li <xin.leo.li@gmail.com>
2022-04-12 21:47:54 +1200

    Populate products in product page.

--- code/mobi-ui/pages/products.tsx
+++ code/mobi-ui/pages/products.tsx
@@ -1,24 +1,35 @@
 import type { NextPage } from 'next';
+import { Product } from '../models/Product';
+import ProductsData from '../services/ProductsData';
+const productDataService = new ProductsData();
+const products = productDataService.getProducts();
+
 const Profile: NextPage = () => {
   return (
-    <div class="columns">
-      <div class="column">
-        <h1 class="bd-notification is-full">Store Name</h1>
-        <div class="columns is-mobile">
-          <div class="column is-one-third">
-            <p class="bd-notification is-danger">Product</p>
-            <p>Example product</p>
-          </div>
-          <div class="column is-half">
-            <p class="bd-notification is-danger">Description</p>
-            <p>Example Description</p>
-          </div>
-          <div class="column is-one-fifth">
-            <p class="bd-notification is-danger">Price</p>
-            <p>example price</p>
+    <div className="columns is-multiline">
+      {products.map((product: Product, index) => (
+        <div key={index} className="column is-2">
+          <div className="card">
+            <div className="card-image">
+              <figure className="image is-4by3">
+                <img
+                  src="https://bulma.io/images/placeholders/1280x960.png"
+                  alt="Placeholder image"
+                />
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="media">
+                <div className="media-content">
+                  <p className="title is-5">{product.Product}</p>
+                  <p className="subtitle is-6">$ {product.Price}</p>
+                </div>
+              </div>
+              <div className="content">{product.Description}</div>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
