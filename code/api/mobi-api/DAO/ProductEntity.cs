6f855698a780020a5eef1bd6d4a448ff28f3730f
Leo LI <xin.leo.li@gmail.com>
2022-04-25 11:20:24 +1200

    install entity framework and create product entity

---
+++ code/api/mobi-api/DAO/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace mobi_api.DAO
+{
+    public class ProductEntity
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Type { get; set; }
+
+        public double? Price { get; set; }
+
+        public double? fee { get; set; }
+    }
+}
---
+++ code/api/mobi-api/Services/MobiConsumerContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mobi_api.DAO;
+
+namespace mobi_api.Services
+{
+    public class MobiConsumerContext : DbContext
+    {
+        public DbSet<ProductEntity> Posts { get; set; }
+        public string DbPath { get; }
+
+        public MobiConsumerContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "mobidbConsumer.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
