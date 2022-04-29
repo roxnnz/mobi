7489307d3b9e4121d192ab742efe6029882d5db5
Leo LI <xin.leo.li@gmail.com>
2022-04-29 23:50:19 +1200

    refactor

--- code/api/mobi-api/Services/DevelopmentData.cs
+++ code/api/mobi-api/Services/DevelopmentData.cs
@@ -1,50 +1,64 @@
 ﻿using System.Text.Json;
 using mobi_api.DAO;
-using mobi_api.Model;
 
 namespace mobi_api.Services
 {
     public interface IDevelopmentData
     {
+        bool CleanDevelopmentData();
         List<StoreEntity> GenerateDevelopmentDataProducts();
         List<StoreEntity> GenerateDevelopmentDataStores();
     }
+
     public class DevelopmentData : IDevelopmentData
     {
-        public DevelopmentData()
+        public readonly MobiConsumerContext _dbContext;
+        public DevelopmentData(MobiConsumerContext mobiConsumerContext)
         {
+            _dbContext = mobiConsumerContext;
+        }
 
+        public bool CleanDevelopmentData()
+        {
+            try
+            {
+                _dbContext.Stores.RemoveRange(_dbContext.Stores);
+                _dbContext.Products.RemoveRange(_dbContext.Products);
+                _dbContext.SaveChanges();
+
+                return true;
+            } catch (Exception ex)
+            {
+                return false;
+            }
+            
         }
 
         public List<StoreEntity> GenerateDevelopmentDataProducts()
         {
             try
             {
-                using (var stream = new StreamReader("MockData/Products.json"))
+                var stream = new StreamReader("MockData/Products.json");
+                string jsonStringProducts = stream.ReadToEnd();
+                List<ProductEntity> products = JsonSerializer.Deserialize<List<ProductEntity>>(jsonStringProducts);
+               
+
+                var stores = _dbContext.Stores.ToList();
+
+                stores.ForEach(store =>
                 {
-                    string jsonStringProducts = stream.ReadToEnd();
-                    List<ProductEntity> products = JsonSerializer.Deserialize<List<ProductEntity>>(jsonStringProducts);
-
-                    using (var db = new MobiConsumerContext())
+                    products.ForEach(product =>
                     {
-                        var stores = db.Stores.ToList();
+                        store.Products.Add(new ProductEntity { ProductName = product.ProductName, Description = product.Description, Price = product.Price, StoreId = store.StoreId });
+                    });
 
-                        stores.ForEach(store =>
-                        {
-                            products.ForEach(product =>
-                            {
-                                store.Products.Add(new ProductEntity { ProductName = product.ProductName, Description = product.Description, Price = product.Price, StoreId = store.StoreId });
-                            });
+                });
 
-                        });
+                _dbContext.SaveChanges();
 
-                        db.SaveChanges();
+                var storess = _dbContext.Stores.ToList();
 
-                        var storess = db.Stores.ToList();
-
-                        return storess;
-                    }
-                }
+                return storess;
             }
             catch (Exception ex)
             {
@@ -56,24 +70,23 @@ namespace mobi_api.Services
         {
             try
             {
-                using (var stream = new StreamReader("MockData/Stores.json"))
+                var stream = new StreamReader("MockData/Stores.json");
+                string jsonStringStores = stream.ReadToEnd();
+
+                List<StoreEntity> stores = JsonSerializer.Deserialize<List<StoreEntity>>(jsonStringStores);
+
+                stores.ForEach(s =>
                 {
-                    string jsonStringStore = stream.ReadToEnd();
-                    List<StoreEntity> stores = JsonSerializer.Deserialize<List<StoreEntity>>(jsonStringStore);
+                    _dbContext.Add(new StoreEntity() { StoreName = s.StoreName, 
+                        PhoneNumber = s.PhoneNumber, 
+                        Address = s.Address, 
+                        Website = s.Website });
+                });
 
-                    using (var db = new MobiConsumerContext())
-                    {
-                        stores.ForEach(s =>
-                        {
-                            db.Add(new StoreEntity() { StoreName = s.StoreName, PhoneNumber = s.PhoneNumber, Address = s.Address, Website = s.Website});
-                        });
+                _dbContext.SaveChanges();
 
-                        db.SaveChanges();
-
-                        var savedStores = db.Stores.ToList();
-                        return savedStores;
-                    }
-                }
+                var savedStores = _dbContext.Stores.ToList();
+                return savedStores;
             }
             catch (Exception ex)
             {
