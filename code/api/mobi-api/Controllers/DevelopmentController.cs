1d71aac70a3a36c39db5e88a0ab772d7a433d0fd
Leo LI <xin.leo.li@gmail.com>
2022-04-29 16:53:57 +1200

    created endpoint to import stores

--- code/api/mobi-api/Controllers/DevelopmentController.cs
+++ code/api/mobi-api/Controllers/DevelopmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using mobi_api.DAO;
+using mobi_api.Services;
 
 namespace mobi_api.Controllers
 {
@@ -7,16 +9,26 @@ namespace mobi_api.Controllers
     [ApiController]
     public class DevelopmentController : ControllerBase
     {
-        public DevelopmentController()
+        private readonly IDevelopmentData _developmentData;
+        public DevelopmentController(IDevelopmentData DevelopmentData)
         {
             // Constractor
+            _developmentData = DevelopmentData;
         }
 
 
-        [HttpPut("generate/testdata/all")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("generate/testdata/stores")]
+        public ActionResult<List<StoreEntity>> Put(int id, [FromBody] string value)
         {
+            
             Console.WriteLine(value);
+            var results = _developmentData.GenereateDevelopmentData();
+            if(results == null)
+            {
+                return Ok(null);
+            }
+            return Ok(results);
+
         }
     }
 }
--- code/api/mobi-api/Services/DevelopmentData.cs
+++ code/api/mobi-api/Services/DevelopmentData.cs
@@ -1,39 +1,47 @@
 ﻿using System.Text.Json;
+using mobi_api.DAO;
 using mobi_api.Model;
 
 namespace mobi_api.Services
 {
-    public class DevelopmentData
+    public interface IDevelopmentData
+    {
+        List<StoreEntity> GenereateDevelopmentData();
+    }
+    public class DevelopmentData : IDevelopmentData
     {
         public DevelopmentData()
         {
 
         }
 
-        public void GenereateDevelopmentData()
+        public List<StoreEntity> GenereateDevelopmentData()
         {
             try
             {
-                using (var stream = new StreamReader("MockData/Users.json"))
-                {
-                    string jsonStringUser = stream.ReadToEnd();
-                    List<User> users = JsonSerializer.Deserialize<List<User>>(jsonStringUser);
-                }
-
-                using (var stream = new StreamReader("MockData/Products.json"))
-                {
-                    string jsonStringProduct = stream.ReadToEnd();
-                    List<Product> products = JsonSerializer.Deserialize<List<Product>>(jsonStringProduct);
-                }
-
-                using (var stream = new StreamReader("MockData/Store.json"))
+                using (var stream = new StreamReader("MockData/Stores.json"))
                 {
                     string jsonStringStore = stream.ReadToEnd();
                     List<Store> stores = JsonSerializer.Deserialize<List<Store>>(jsonStringStore);
+
+                    using (var db = new MobiConsumerContext())
+                    {
+                        stores.ForEach(s =>
+                        {
+                            db.Add(new StoreEntity() { StoreName = s.StoreName, PhoneNumber = s.PhoneNumber, Address = s.Address, WebSite = s.Website });
+                        });
+
+                        db.SaveChanges();
+
+                        var products = db.Stores.ToList();
+                        return products;
+                    }
                 }
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex.Message); 
+                return null;
             }
         }
     }
