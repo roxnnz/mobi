06d21ecbb019e3d30da5a1f3a392a69e89433f37
Jimmy Mo <jimmymonz@gmail.com>
2022-06-23 21:51:46 +1200

    Created endpoint Update Product by ProductId using patch and DTO.

---
+++ code/api/mobi-api/Dtos/UpdateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mobi_api.Dtos
+{
+    //Receive data from request
+    public record UpdateProductDto
+    {
+        [Required]
+        public string? ProductName { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required, Range(0.00, 1000.00)]
+        public double Price { get; set; }
+    }
+}
